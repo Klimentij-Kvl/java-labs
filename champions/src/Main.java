0c24d6a665c8a55f6c05643e594f10e838a9ecce
Klimentiy <klimentijkvlvsk@gmail.com>
2024-12-11 18:33:51 +0300

    delete some trash

--- champions/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        creationList progr = new creationList();
-        progr.readFile("in.txt");
-        progr.writeFileToConsole();
-    }
-}
--- tableTest/src/main/java/org/example/tabletest/Employee.java
+++
@@ -1,4 +0,0 @@
-package org.example.tabletest;
-
-public class Employee {
-}
