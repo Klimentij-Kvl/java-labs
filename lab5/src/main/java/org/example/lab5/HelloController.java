f2b24c278512c3da8ed559306bdbc040cf7dd9c4
Klimentij-Kvl <klimentijkvlvsk@gmail.com>
2024-12-11 19:18:05 +0300

    lab5 fully done

--- lab5/src/main/java/org/example/lab5/HelloController.java
+++ lab5/src/main/java/org/example/lab5/HelloController.java
@@ -17,6 +17,10 @@ public class HelloController {
 
     @FXML
     void initialize(){
+        group1 = new ToggleGroup();
+        group2 = new ToggleGroup();
+        group3 = new ToggleGroup();
+
         pozd1.setToggleGroup(group1);
         pozd2.setToggleGroup(group1);
         pozd3.setToggleGroup(group1);
@@ -31,8 +35,22 @@ public class HelloController {
     }
 
     @FXML
-    void click(){
+    void buttonClicked(){
         int a,b,c;
-        if(pozd1.)
+        if(pozd1.isSelected()) a = 1000;
+        else if(pozd2.isSelected()) a = 1500;
+        else if(pozd3.isSelected()) a = 3000;
+        else a = 0;
+
+        if(gift1.isSelected()) b = 500;
+        else if(gift2.isSelected()) b = 800;
+        else if(gift3.isSelected()) b = 50;
+        else if(gift4.isSelected()) b= 70;
+        else b = 0;
+
+        if(yes.isSelected()) c = 15000;
+        else c = 0;
+
+        label.setText(String.valueOf(a+b+c));
     }
 }
